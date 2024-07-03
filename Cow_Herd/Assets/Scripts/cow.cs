a9c27b62a7fbe8e9d0f64b02a08627e533f43b8f
SpaceDino <al.dev.knight@gmail.com>
2024-07-03 18:24:44 +0300

    Cow can now move
    added a function that moves the cow to a target

--- Cow_Herd/Assets/Scripts/cow.cs
+++ Cow_Herd/Assets/Scripts/cow.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class cow : MonoBehaviour
 {
+    [SerializeField] Transform TESTING;
     [SerializeField] private float healthPoints = 100;
+    [SerializeField] private float cowSpeed = 2f;
+
     private float milkPoints = 0;
+
+    Rigidbody cowRigidBody;
+    void MoveCowTo(Transform target)
+    {
+        Vector3 direction = -(this.transform.position - target.position).normalized;
+        cowRigidBody.MovePosition(this.transform.position + direction*cowSpeed*Time.deltaTime);    
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        cowRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveCowTo(TESTING);
+
+    }
+    private void FixedUpdate()
+    {
+
     }
 }
